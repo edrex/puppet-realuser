96ab25c78b8b517e97fa5391ff2f796301e80f92
Eric Drechsel <eric@sqrly.com>
2010-08-16 23:50:35 +0000

    fix indentation

--- manifests/defines/realuser.pp
+++ manifests/defines/realuser.pp
@@ -2,31 +2,31 @@
 
 define realuser($uid, $groups=[], $comment, $key, $keytype="ssh-rsa") {
     group { $name :
-	gid	=> $uid,
+        gid     => $uid,
     }
 
     user { $name :
-	uid	=> $uid,
-	gid	=> $uid,
-	groups  => $groups,
-	shell   => "/bin/bash",
-	comment => $comment,
-	home	=> "/home/$name",
-	managehome => true,
-	require => Group[$name],
+        uid     => $uid,
+        gid     => $uid,
+        groups  => $groups,
+        shell   => "/bin/bash",
+        comment => $comment,
+        home    => "/home/$name",
+        managehome => true,
+        require => Group[$name],
     }
     
     ssh_authorized_key { $name :
-	user	=> $name,
-	key	=> $key,
-	type	=> $keytype,
+        user    => $name,
+        key     => $key,
+        type    => $keytype,
     }
 
     # require password change on first login
     # WARNING: will unlock locked accounts, TODO fix that
     exec { "usermod -p '' $name; chage -d 0 $name":
-        onlyif	=> "test `passwd -S $name|cut -d ' ' -f 2` == L",
-	require => User[$name],
+        onlyif  => "test `passwd -S $name|cut -d ' ' -f 2` == L",
+        require => User[$name],
     }
 }
-	
+        
